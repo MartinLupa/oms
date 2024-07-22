e001a97c8b5c6e29b439f593cf8721b3637f3528
Martin Lupa <marlupa@dfds.com>
2024-07-22 20:51:43 +0200

    chore: upgrade provider

--- microservices/order-processing/terraform/provider.tf
+++ microservices/order-processing/terraform/provider.tf
@@ -26,7 +26,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.0"
     }
     archive = {
       source  = "hashicorp/archive"
